d33d155fae94528e8fe1b54886cfe9018fce9cd8
McKenna <mckenna.j.pasquale@gmail.com>
2025-01-09 18:48:30 -0500

    Added javadoc comments to Main.java in javadoc-comments branch

--- Main.java
+++ Main.java
@@ -1,4 +1,17 @@
-public class Main {
+/**
+ * A utility class that provides methods to calculate
+ * the Fibonacci se
+            uence using recursion. 
+ * <p>
+ * This class demonstrates the use of recursive algorithms
+ * for educational purposes
+ * 
+ * @param n the position in the Fibonacci sequence where n>= 0
+ * @return the nth Fibonacci number
+ * @throws IllegalArgumentExceoption if n is negative
+ */
+
+ public class Main {
     public static int fibonacci(int n) {
         if (n <= 1) return n;
         return fibonacci(n - 1) + fibonacci(n - 2);
